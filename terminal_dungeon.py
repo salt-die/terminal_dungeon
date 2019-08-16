9f959ce96ddf85403973f77c31a1aff725a8dfe9
salt-die <naclmolecule@gmail.com>
2019-08-16 18:03:54 -0500

    typo

--- terminal_dungeon.py
+++ terminal_dungeon.py
@@ -11,7 +11,7 @@ Make sure the pygame window is focused for input events to be received.
 Depending on your terminal font, Renderer.ascii_map may need to be adjusted.
 
 Values stored in textures should range from 0-9.  Values below 6 are
-substractive and above 6 are additive.
+subtractive and above 6 are additive.
 
 If you'd like to make an ascii map more suitable to your terminal's font,
 check my Snippets repository for a script that grabs mean brightness of
