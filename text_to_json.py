b733e91970194ccbfa623704db8bb4a960fd5b23
salt-die <naclmolecule@gmail.com>
2019-08-20 23:56:44 -0500

    prettier json files

--- text_to_json.py
+++ text_to_json.py
@@ -6,7 +6,7 @@ import json
 def text_to_json(filename):
     with open(filename + ".txt", "r") as text:
         pre_format = [list(row) for row in text.read().splitlines()]
-        jsontext = json.dumps(pre_format)
+        jsontext = json.dumps(pre_format, indent=4)
 
     with open(filename + ".json", "w") as new_file:
         new_file.write(jsontext)
