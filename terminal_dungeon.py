8d6dbe7aeac48e9c43a8f5d387ce20c236e6c295
salt-die <naclmolecule@gmail.com>
2019-10-25 17:11:03 -0500

    class constants moved outside __init__

--- terminal_dungeon.py
+++ terminal_dungeon.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 A terminal based ray-casting engine.
 
@@ -22,6 +21,13 @@ import curses
 from pynput import keyboard
 from pynput.keyboard import Key, KeyCode
 
+
+def rotation_matrix(theta):
+    """
+    Returns a 2-dimensional rotation array of a given angle.
+    """
+    return np.array([[np.cos(theta), np.sin(theta)],
+                     [-np.sin(theta), np.cos(theta)]])
 
 class Map:
     """
@@ -51,27 +57,18 @@ class Player:
     Player class with methods for moving and updating any effects on the
     player such as falling.
     """
-    def __init__(self, game_map, pos=np.array([5., 5.]),
-                 initial_angle=0):
-        #Settings======================================================
-        self.speed = .1
-        self.rotate_speed = .05
-        self.jump_time = 8
-        self.field_of_view = .6 #Somewhere between 0 and 1 is reasonable
+    speed = .1
+    rotate_speed = .05
+    jump_time = 8
+    field_of_view = .6  # Somewhere between 0 and 1 is reasonable
 
+    left = rotation_matrix(-rotate_speed)
+    right = rotation_matrix(rotate_speed)
+
+    def __init__(self, game_map, pos=np.array([5., 5.]), initial_angle=0):
         self.game_map = game_map
         self.pos = pos
-        self.cam = np.array([[1, 0], [0, self.field_of_view]]) @\
-                   np.array([[np.cos(initial_angle), np.sin(initial_angle)],
-                             [-np.sin(initial_angle), np.cos(initial_angle)]])
-        self.left = np.array([[np.cos(-self.rotate_speed),
-                               np.sin(-self.rotate_speed)],
-                              [-np.sin(-self.rotate_speed),
-                                np.cos(-self.rotate_speed)]])
-        self.right = np.array([[np.cos(self.rotate_speed),
-                                np.sin(self.rotate_speed)],
-                               [-np.sin(self.rotate_speed),
-                                np.cos(self.rotate_speed)]])
+        self.cam = np.array([[1, 0], [0, self.field_of_view]]) @ rotation_matrix(initial_angle)
         self.perp = np.array([[0., -1.],
                               [1., 0.]])
         self.time_in_jump = 0
@@ -79,7 +76,7 @@ class Player:
         self.is_jumping = False
 
     def update(self):
-        #We'll have more to do here eventually.
+        # We'll have more to do here eventually.
         self.fall()
 
     def fall(self):
@@ -100,7 +97,7 @@ class Player:
         next_step = self.pos + speed * \
                     (self.cam[0] @ self.perp if strafe else self.cam[0])
 
-        #If we can move both coordinates at once, we should
+        # If we can move both coordinates at once, we should
         if not self.game_map[tuple(next_step.astype(int))]:
             self.pos = next_step
 
@@ -213,44 +210,44 @@ class Renderer:
         return line_start, line_end, self.ascii_map[shade_buffer]
 
     def cast_sprites(self):
-        #For each sprite, calculate distance (squared) to player
+        # For each sprite, calculate distance (squared) to player
         sprite_distances = {}
         for i, sprite in enumerate(self.game_map.sprites):
-            #Relative position of sprite to player
+            # Relative position of sprite to player
             sprite["relative"] = self.player.pos - sprite["pos"]
             sprite_distances[i] = sprite["relative"] @ sprite["relative"]
 
-        #Sprites sorted by distance from player.
+        # Sprites sorted by distance from player.
         sorted_sprites = sorted(sprite_distances, key=sprite_distances.get, reverse=True)
         sorted_sprites = [self.game_map.sprites[i] for i in sorted_sprites]
 
-        #Camera Inverse used to calculate transformed position of sprites.
+        # Camera Inverse used to calculate transformed position of sprites.
         cam_inv = np.linalg.inv(-self.player.cam[::-1])
 
-        #Draw each sprite from furthest to closest.
+        # Draw each sprite from furthest to closest.
         for sprite in sorted_sprites:
-            #Transformed position of sprites due to camera's plane and angle
+            # Transformed position of sprites due to camera's plane and angle
             trans_pos = sprite["relative"] @ cam_inv
 
-            if trans_pos[1] <= 0: #Sprite is behind player, don't draw it.
+            if trans_pos[1] <= 0:  # Sprite is behind player, don't draw it.
                 continue
 
-            #Sprite x-position on screen
+            # Sprite x-position on screen
             sprite_x = int(self.height * (1 + trans_pos[0] / trans_pos[1]) - 1)
-            #Sprite width and height
+            # Sprite width and height
             sprite_height = int(self.height / trans_pos[1])
             sprite_width = int(self.width / trans_pos[1] / 2)
             if not all([sprite_height, sprite_width]): #Sprite too small.
                 continue
 
-            #Start and end points of vertical lines of the sprite
+            # Start and end points of vertical lines of the sprite
             start_y, end_y = [int((i * sprite_height + self.height) / 2
                               + self.player.z * sprite_height)
                               for i in [-1, 1]]
             if start_y < 0: start_y = 0
             if end_y >= self.height: end_y = self.height
 
-            #Start and end points of horizontal lines
+            # Start and end points of horizontal lines
             start_x, end_x = [(i * sprite_width // 2 + sprite_x)
                               for i in [-1, 1]]
             if start_x < 0: start_x = 0
@@ -258,49 +255,48 @@ class Renderer:
 
             tex_width, tex_height = self.textures[sprite["image"]].shape
 
-            #Calculate some constants outside the next loops:
+            # Calculate some constants outside the next loops:
             clip_x = sprite_x - sprite_width / 2
             clip_y = (sprite_height - self.height) / 2\
                       - self.player.z * sprite_height
             width_ratio = tex_width / sprite_width
             height_ratio = tex_height / sprite_height
 
-            #Draw sprite -- outer-loop, left-to-right; inner, top-to-bottom
+            # Draw sprite -- outer-loop, left-to-right; inner, top-to-bottom
             for column in range(start_x, end_x):
-                #From which column in the texture characters are taken
+                # From which column in the texture characters are taken
                 tex_x = int((column - clip_x) * width_ratio)
 
-                #Check that column isn't off-screen and that sprite isn't
-                #blocked by a wall
+                # Check that column isn't off-screen and that sprite isn't blocked by a wall.
                 if 0 <= column <= self.width and\
                    trans_pos[1] <= self.distances[column]:
 
                     vertical_buffer = [0] * (end_y - start_y)
 
                     for i in range(start_y, end_y):
-                        #From which row characters are taken
+                        # From which row characters are taken
                         tex_y = int((i + clip_y) * height_ratio)
                         char = self.textures[sprite["image"]][tex_x, tex_y]
                         vertical_buffer[i - start_y] = char\
                             if char != "0" else self.buffer[i, column]
 
-                    #Convert to array to broadcast into buffer
+                    # Convert to array to broadcast into buffer
                     vertical_buffer = np.array(vertical_buffer, dtype=str)
                     self.buffer[start_y:end_y, column] = vertical_buffer
 
     def update(self):
-        #Clear buffer
+        # Clear buffer
         self.buffer = np.full((self.height, self.width), " ", dtype=str)
 
-        #Draw floor
+        # Draw floor
         self.buffer[self.floor_y:, :] = self.ascii_map[1]
 
-        #Draw walls
+        # Draw walls
         for column in range(self.width - 1):
             start, end, col_buffer = self.cast_ray(column)
             self.buffer[start:end, column] = col_buffer
 
-        #Draw sprites
+        # Draw sprites
         self.cast_sprites()
 
         #Push buffer to screen
@@ -342,14 +338,14 @@ class Controller():
         self.keys[key] = False
 
     def movement(self):
-        #We stop accepting move inputs (but turning is ok) in the middle of a
-        #jump -- the effect is momentum-like movement while in the air.
+        # We stop accepting move inputs (but turning is ok) in the middle of a
+        # jump -- the effect is momentum-like movement while in the air.
         keys = self.jumping_keys if self.player.is_jumping else self.keys
         if self.player_has_jumped:
             self.jumping_keys = self.keys.copy()
             self.player_has_jumped = False
 
-        #Constants that make the following conditionals much more readable
+        # Constants that make the following conditionals much more readable
         left = self.keys[Key.left] or self.keys[KeyCode(char='a')]
         right = self.keys[Key.right] or self.keys[KeyCode(char='d')]
         up = keys[Key.up] or keys[KeyCode(char='w')]
@@ -377,7 +373,7 @@ def main(screen):
     init_curses(screen)
     game_map = Map("map1")
     player = Player(game_map)
-    #We may mass load textures in the future and pass the list to renderer.
+    # We may mass load textures in the future and pass the list to renderer.
     renderer = Renderer(screen, player, game_map,
                         "texture1", "texture2", "texture3")
     controller = Controller(player, renderer)
